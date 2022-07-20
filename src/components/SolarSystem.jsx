22763ccf692471dab3d68ca143ed3fc66e428c4b
lucas-da-silva <lucassouza00012345@gmail.com>
2022-07-20 16:01:47 -0300

    sixth equirement done

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (<PlanetCard
+          key={ name }
+          planetName={ name }
+          planetImage={ image }
+        />))}
       </div>
     );
   }
